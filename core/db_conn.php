ae4d41f779f7e361eec11c7fe9855cbadefa604c
leesua <s2332@e-mirim.hs.kr>
2025-06-15 23:04:22 +0900

    환경변수 dotenv → getenv 방식으로 변경

--- core/db_conn.php
+++ core/db_conn.php
@@ -1,13 +1,13 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php'; 
+//require_once __DIR__ . '/../vendor/autoload.php'; 
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+// $dotenv->load();
 
-$host = $_ENV['DB_HOST'];
-$database = $_ENV['DB_NAME'];
-$id = $_ENV['DB_USER'];
-$pw = $_ENV['DB_PASS'];
+$host = getenv('DB_HOST');
+$database = getenv('DB_NAME');
+$id = getenv('DB_USER');
+$pw = getenv('DB_PASS');
 
 try {
     $dsn = "mysql:host={$host};dbname={$database};charset=utf8mb4";
