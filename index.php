a8a1af972954dceb337785720a3f29691cfc723e
leesua <s2332@e-mirim.hs.kr>
2025-06-18 16:05:17 +0900

    CORS 설정 메소드 추가

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ header("Access-Control-Allow-Origin: $origin");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, FATCH, DELETE, OPTIONS");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
