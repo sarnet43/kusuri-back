6fdf23ff20157babac52b696633c3bba5948ebc6
leesua <s2332@e-mirim.hs.kr>
2025-06-18 19:01:01 +0900

    origin 변경

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_set_cookie_params([
-    'lifetime' => 43200, 
-    'secure' => true,
+    'lifetime' => 43200,
+    'secure' => false,
     'path' => '/',
     'httponly' => true,
-    'samesite' => 'None'
+    'samesite' => 'Lax'
 ]);
 session_start();
 
@@ -12,12 +12,12 @@ error_log('현재 경로: ' . $_SERVER['REQUEST_URI']);
 error_log('받은 쿠키: ' . print_r($_COOKIE, true));
 error_log('세션 ID: ' . session_id());
 
-$origin = "https://kusuri-green.vercel.app";
+$origin = "http://localhost:3000";
 header("Access-Control-Allow-Origin: $origin");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-header("Access-Control-Allow-Methods: GET, POST, FATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
